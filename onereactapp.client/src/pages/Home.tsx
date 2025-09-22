20db312308f7ff7c2eeaf4e020dfd01845cca99e
SunChao Dong <dongsunchao@gmail.com>
2025-09-22 22:46:23 +1000

    update for the homepage in react
    Signed-off-by: SunChao Dong <dongsunchao@gmail.com>

--- onereactapp.client/src/pages/Home.tsx
+++ onereactapp.client/src/pages/Home.tsx
@@ -1,12 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link, Routes, Route } from 'react-router-dom';
+
+interface HomeProp{
+    username: string;
+    userId: number;
+    UserState: string; // Assuming
+}
 
 function Home() {
+    const [homeState, setHomeState] = React.useState<HomeProp[]>();
+
+    useEffect(() => {
+        LoadHomeData();
+    }, []);
+
+    const contents = homeState === undefined
+        ? <p><em>Loading...</em></p>
+        : <table className="table table-striped" aria-labelledby="tableLabel">
+            <thead>
+                <tr>
+                    <th>Username</th>
+                    <th>UserId</th>
+                    <th>UserState</th>
+                </tr>
+            </thead>
+            <tbody>
+                {homeState.map(home =>
+                    <tr key={home.userId}>
+                        <td>{home.username}</td>
+                        <td>{home.userId}</td>
+                        <td>{home.UserState}</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>;
+
+
     return (
         <div>
-            <h1>Welcome to React!</h1>
-            <p>This is a simple React application.</p>
+            <h1 id="tableLabel">Home Page</h1>
+            {contents}
         </div>
     );
+
+    async function LoadHomeData() {
+        const response = await fetch('home');
+        if (response.ok) {
+            const data = await response.json();
+            setHomeState(data);
+        }
+    }
 }
 
 export default Home;
